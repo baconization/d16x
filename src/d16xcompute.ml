6f5db4460458c253884a56f20eb071038ef31355
Bacon God <baconizer@bacon.com>
2012-04-07 10:55:45 -0700

    refactored and made slower (for future sake) stackcompute() so the hairy pattern matching is outside of the soon to explode in complexity compile process

--- src/d16xcompute.ml
+++ src/d16xcompute.ml
@@ -5,36 +5,64 @@ open D16xparse;;
 open D16xutil;;
 module StringMap = Map.Make (String);;
 
-let compile_computes tree =
-   (* stackcompute is a very simple algorithm without any kind of trash registers to optimize the computation *)
-   let rec stackcompute tree loc highest = (
-      let lookup_operator op =
-         match op with
-            "+" -> (true, "ADD")
-         |  "-" -> (true, "SUB")
-         |  "*" -> (true, "MUL")
-         |  "/" -> (true, "DIV")
-         |  "%" -> (true, "MOD")
-         | _ -> (false, "") in
-      match tree with
-         TreeExpression(op, [a;b]) ->
-            let (is_op, operand) = lookup_operator op in
+type tyCompute = Const of int | Expr of string*bool*tyCompute*tyCompute | Value of tyTree;;
+
+(* this encodes our tree structure with enough metadata to simplify the optimization problem *)
+let rec pre_formulate_and_compute tree =
+   let lookup_operator op =
+      match op with
+         "+" -> (true, "ADD", true)
+      |  "-" -> (true, "SUB", false)
+      |  "*" -> (true, "MUL", true)
+      |  "/" -> (true, "DIV", false)
+      |  "%" -> (true, "MOD", false)
+      | _ -> (false, "", false) in
+   match tree with
+      TreeInteger(n) -> Value(tree)
+   |  TreeExpression(op, [a;b]) ->
+      let left = pre_formulate_and_compute a in
+      let right = pre_formulate_and_compute b in
+         (match (op, left, right) with
+            ("+", Const(v0), Const(v1)) -> Const(v0 + v1)
+         |  ("-", Const(v0), Const(v1)) -> Const(v0 - v1)
+         |  ("*", Const(v0), Const(v1)) -> Const(v0 * v1)
+         |  ("/", Const(v0), Const(v1)) -> Const(v0 / v1)
+         |  _ -> let (is_op, operation, commutes) = lookup_operator op in
             if is_op then
-               let eval_a = stackcompute a loc false in
-               let eval_b = stackcompute b loc false in
-               let x1 = TreeExpression("SET", [loc;TreeIdent("POP")]) in
-               let x2 = TreeExpression(operand, [loc;TreeIdent("POP")]) in
-               let x3 = if highest then [] else [TreeExpression("SET", [TreeIdent("PUSH");loc])] in
-                  TreeExpression("SEQ", [eval_b;eval_a;x1;x2] @ x3)
+               Expr(operation, commutes, left, right)
             else
-               TreeExpression("SET", [TreeIdent("PUSH");tree])
-      |  _-> TreeExpression("SET", [TreeIdent("PUSH");tree])) in
+               Value(tree))
+   |  _ -> Value(tree)
+   ;;
+
+let rec next_stack_compute atree loc =
+   match atree with
+      Const(v) -> TreeExpression("SET", [TreeIdent("PUSH");TreeInteger(v)])
+   |  Value(t) -> TreeExpression("SET", [TreeIdent("PUSH");t])
+   |  Expr(operation, commutes, left, right) ->
+      let eval_left = next_stack_compute left loc in
+      let eval_right = next_stack_compute right loc in
+      let x1 = TreeExpression("SET", [loc;TreeIdent("POP")]) in
+      let x2 = TreeExpression(operation, [loc;TreeIdent("POP")]) in
+      let x3 = [TreeExpression("SET", [TreeIdent("PUSH");loc])] in
+         TreeExpression("SEQ", [eval_right;eval_left;x1;x2] @ x3)
+   ;;
+
+(*
+   eventually, I'll introduce this concept
+   let trash = [TreeExpression("REG", [TreeIdent("X")]);TreeExpression("REG", [TreeIdent("Y")]);TreeExpression("REG", [TreeIdent("Z")])] in
+
+   where I can pull and put registers/locations so I can optimize the stack utilization
+*)
+
+let stackcompute tree loc =
+   let atree = pre_formulate_and_compute tree in
+   next_stack_compute atree loc;;
+
+let compile_computes tree =
    let find = function x -> match x with ("stackcompute",[a;b]) -> true | _ -> false in
-   let replace = function x -> match x with ("stackcompute",[a;b]) -> stackcompute b a true | _ -> tree in
+   let replace = function x -> match x with ("stackcompute",[a;b]) -> stackcompute b a | _ -> tree in
       rewrite tree find replace;;
-
-
-
 
 
 
